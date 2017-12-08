778899c41a958ad409374fcddc12e9962fb009d6
andjc <lang.suport@gmail.com>
2017-12-09 06:59:55 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# globalbility
+Elfdalian and Togolese Keyman kbd layouts for the Globalbility Project
+# adlham
---
+++ js/kmwinit.js
@@ -0,0 +1,27 @@
+/***
+   KeymanWeb 2.0
+   Copyright 2017 SIL International
+***/
+
+/********************************************************/
+/*                                                      */
+/* Automatically initialize keymanweb with defaults     */ 
+/* after the page is fully loaded                       */ 
+/*                                                      */
+/********************************************************/
+
+(function()
+{
+  // Declare KeymanWeb object
+  var keymanweb=window['tavultesoft']['keymanweb'];
+
+  // We don't want to instantly init() in case this code is used via bookmarklet.
+  var readyStateCheckInterval = window.setInterval(function() {
+    if (document.readyState === "complete") 
+    {
+      window.clearInterval(readyStateCheckInterval);
+      keymanweb.init(null);
+    }
+  }, 10);    
+ 
+})();
